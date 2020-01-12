af77ee5de171a9821b57196ba552e790a90ec9d6
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-01-13 00:22:17 +0200

    Add another test with different sample inputs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "clique-router.cpp"
 
-int test_step_4_and_5() {
+int test1() {
     vector<Node*> nodes;
 
     auto n1 = new Node(0);
@@ -38,6 +38,45 @@ int test_step_4_and_5() {
     return 0;
 }
 
+int test2() {
+    vector<Node*> nodes;
+
+    auto n1 = new Node(0);
+    vector<int> v = {1, 2, 3, 0};
+    n1->add_messages(v);
+    nodes.push_back(n1);
+
+    auto n2 = new Node(1);
+    v = {0, 1, 2, 3};
+    n2->add_messages(v);
+    nodes.push_back(n2);
+
+    auto n3 = new Node(2);
+    v = {2, 3, 0, 1};
+    n3->add_messages(v);
+    nodes.push_back(n3);
+
+    auto n4 = new Node(3);
+    v = {3, 2, 1, 0};
+    n4->add_messages(v);
+    nodes.push_back(n4);
+
+    CliqueRouter cr;
+    cr.route(nodes);
+
+    for (auto node : nodes) {
+        int global_id = node->get_node_idx();
+        auto messages = node->get_messages();
+
+        for (auto m : messages) {
+            assert(m->dest == global_id);
+        }
+    }
+
+    return 0;
+}
+
 int main() {
-    test_step_4_and_5();
+    test1();
+    test2();
 }
