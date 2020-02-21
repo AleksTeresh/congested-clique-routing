faa19daad06d8ceac435520473491a0b9eee9cea
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-02-21 22:17:45 +0200

    Change build.sh to delete all files in build/ dir except for .gitignore

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-rm -rf build/
-mkdir build
 cd build
+find . ! -name '.gitignore' -type f -exec rm -f {} + # delete all files from ./build/ other than .gitignore
 em++ ../cpp/main.cpp ../cpp/node.cpp --bind -s WASM=1 -std=c++17 -s -o main.js || exit 1
 mv main.js ../web/gen/
 mv main.wasm ../web/gen/
