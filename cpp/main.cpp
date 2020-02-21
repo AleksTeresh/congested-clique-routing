d9a4a1513f4748aaaa469c943a4e726fd3949b40
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-02-22 00:27:48 +0200

    Fix formatting

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,18 +9,18 @@ EMSCRIPTEN_BINDINGS(my_module) {
                 .field("final_dest", &Message::final_dest);
 
         value_object<MessageCount>("MessageCount")
-        .field("msg_src", &MessageCount::msg_src)
-        .field("msg_dest", &MessageCount::msg_dest)
-        .field("msg_count", &MessageCount::msg_count)
-        .field("info_dest", &MessageCount::info_dest);
+                .field("msg_src", &MessageCount::msg_src)
+                .field("msg_dest", &MessageCount::msg_dest)
+                .field("msg_count", &MessageCount::msg_count)
+                .field("info_dest", &MessageCount::info_dest);
 
         value_object<NodeData>("NodeData")
-        .field("id", &NodeData::id)
-        .field("messages", &NodeData::messages)
-        .field("message_counts", &NodeData::message_counts);
+                .field("id", &NodeData::id)
+                .field("messages", &NodeData::messages)
+                .field("message_counts", &NodeData::message_counts);
 
         value_object<TimePoint>("TimePoint")
-        .field("nodes", &TimePoint::nodes);
+                .field("nodes", &TimePoint::nodes);
 
         register_vector<TimePoint>("vector<TimePoint>");
         register_vector<NodeData>("vector<NodeData>");
