d2a9254e4f1145b1ef1c67bc211c086a8e86f485
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-02-22 00:06:36 +0200

    Fix build script to create web/gen/ directory if missing

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cd build
 find . ! -name '.gitignore' -type f -exec rm -f {} + # delete all files from ./build/ other than .gitignore
 em++ ../cpp/main.cpp ../cpp/node.cpp --bind -s WASM=1 -std=c++17 -s -o main.js || exit 1
+mkdir -p ../web/gen/
 mv main.js ../web/gen/
 mv main.wasm ../web/gen/
