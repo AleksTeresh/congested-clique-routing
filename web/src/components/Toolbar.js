f08ff17b0823e1bf8db6f63cb0fe06d283deea30
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-02-27 22:03:00 +0200

    Add some margins to UI

--- web/src/components/Toolbar.js
+++ web/src/components/Toolbar.js
@@ -12,22 +12,25 @@ export default function Toolbar({
   loadData
 }) {
   return (
-    <>
-      <div>
+    <div style={{ margin: '80px', marginTop: '20px' }}>
+      <div style={{ margin: '20px' }}>
         <button
+          style={{ margin: '5px' }}
           disabled={round === 0}
           onClick={handlePrev}
-          id="prevBtn">Prev</button>
+          id="prevBtn">Prev round</button>
         <button
+          style={{ margin: '5px' }}
           disabled={round === NUM_OF_ROUNDS}
           onClick={handleNext}
-          id="nextBtn">Next</button>
+          id="nextBtn">Next round</button>
       </div>
       <div>
         <label id="roundLabel"></label>
       </div>
-      <div>
-        <label id="numberOfNodes">{'Round ' + tentativeSetSize}</label>
+      <div style={{ margin: '20px' }}>
+        <label id="numberOfNodes">{'Size of subset ' + tentativeSetSize}</label>
+        <br />
         <input
           onInput={(ev) => setTentativeSetSize(Number(ev.target.value))}
           type="range"
@@ -37,14 +40,14 @@ export default function Toolbar({
           className="slider"
           id="nodesRange" />
       </div>
-      <div>
+      <div style={{ margin: '20px' }}>
         <button
           onClick={() => {
             setSetSize(Number(tentativeSetSize))
             loadData(Number(tentativeSetSize))
           }}
-          id="computeBtn">Compute</button>
+          id="computeBtn">Compute routing</button>
       </div>
-    </>
+    </div>
   )
 }
