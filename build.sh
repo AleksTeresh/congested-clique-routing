88b216b5d4de498bc24d44b50383731c6683927b
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2020-03-05 18:19:25 +0200

    Remove build dir from git, add build dir creation to build.sh script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p ./build
 cd build
 find . ! -name '.gitignore' -type f -exec rm -f {} + # delete all files from ./build/ other than .gitignore
 em++ ../cpp/main.cpp ../cpp/node.cpp --bind -s WASM=1 -s MODULARIZE=1 -s 'EXPORT_NAME="InitRuntime"' -s ENVIRONMENT=web -std=c++17 -s -o main.js || exit 1
